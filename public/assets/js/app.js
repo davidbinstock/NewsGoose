461ec4e5e17821de4c4fd6f995335252182c8d65
davidbinstock <binstock.d@gmail.com>
2018-06-23 16:34:22 -0500

    home and articles page links working, scrape button set up

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,1 +1,8 @@
-console.log("Hey we're all connected here in the app.js file!")
+// scrape button event handler
+$("#scrape-button").on("click", () => {
+    console.log("you clicked scrape!")
+    $.get("/scrape", () => {
+        console.log("scrape complete");
+        location.href = "/articles"
+    })
+})
